f4cb2cad2867f5bb384b6423261b797921b81f59
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2012-11-15 11:04:05 +0100

    Including the files that are in the current build of the NuGet package

---
+++ build/T4TS.cs
@@ -0,0 +1,14 @@
+﻿namespace T4TS
+{
+    using System;
+    using System.CodeDom;
+    using System.CodeDom.Compiler;
+    
+    [GeneratedCode("T4TS", "0.1")]
+    public class TypeScriptInterfaceAttribute : Attribute
+    {
+        public TypeScriptInterfaceAttribute()
+        {
+        }
+    }
+}
