d992b19e856f505daf56e4662441702d9f107fa5
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-13 00:22:58 +0100

    Add regression test for NumberTypes

--- T4TS.Tests/TypeContext/TypeContextTests.cs
+++ T4TS.Tests/TypeContext/TypeContextTests.cs
@@ -53,5 +53,32 @@ namespace T4TS.Tests
             var resolvedType = context.GetTypeScriptType(typeof(DateTimeOffset).FullName);
             Assert.IsInstanceOfType(resolvedType, typeof(StringType));
         }
+
+        [TestMethod]
+        public void ShouldSupportNumberTypes()
+        {
+            var inputTypes = new [] {
+                typeof(byte),
+                typeof(sbyte),
+                typeof(short),
+                typeof(ushort),
+                typeof(int),
+                typeof(uint),
+                typeof(long),
+                typeof(ulong),
+                typeof(float),
+                typeof(decimal),
+                typeof(double)
+            };
+
+            var context = new TypeContext(new Settings());
+            var expectedType = typeof(NumberType);
+
+            foreach (var type in inputTypes)
+            {
+                var resolvedType = context.GetTypeScriptType(type.FullName);
+                Assert.IsInstanceOfType(resolvedType, expectedType);
+            }
+        }
     }
 }
