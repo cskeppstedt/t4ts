08091731974d76c4b49560465728990806a8804d
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-13 00:14:10 +0100

    Add support for datetime(offset)

--- T4TS/TypeContext.cs
+++ T4TS/TypeContext.cs
@@ -129,6 +129,7 @@ namespace T4TS
                     return new StringType();
 
                 case "System.DateTime":
+                case "System.DateTimeOffset":
                     if (Settings.UseNativeDates)
                         return new DateTimeType();
                     else
