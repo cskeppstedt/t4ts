ea95b7630079233b8c2e3681c4743223771dea1c
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-13 00:14:04 +0100

    Add tests for datetime(offset)

---
+++ T4TS.Tests/TypeContext/TypeContextTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace T4TS.Tests
+{
+    [TestClass]
+    public class TypeContextTests
+    {
+        [TestMethod]
+        public void ShouldSupportDatetimesAsNativeDates()
+        {
+            var context = new TypeContext(new Settings
+            {
+                UseNativeDates = true
+            });
+
+            var resolvedType = context.GetTypeScriptType(typeof(DateTime).FullName);
+            Assert.IsInstanceOfType(resolvedType, typeof(DateTimeType));
+        }
+
+        [TestMethod]
+        public void ShouldSupportDatetimesAsStrings()
+        {
+            var context = new TypeContext(new Settings
+            {
+                UseNativeDates = false
+            });
+
+            var resolvedType = context.GetTypeScriptType(typeof(DateTime).FullName);
+            Assert.IsInstanceOfType(resolvedType, typeof(StringType));
+        }
+
+        [TestMethod]
+        public void ShouldSupportDatetimeOffsetAsNativeDates()
+        {
+            var context = new TypeContext(new Settings 
+            {
+                UseNativeDates = true
+            });
+
+            var resolvedType = context.GetTypeScriptType(typeof(DateTimeOffset).FullName);
+            Assert.IsInstanceOfType(resolvedType, typeof(DateTimeType));
+        }
+
+        [TestMethod]
+        public void ShouldSupportDatetimeOffsetAsStrings()
+        {
+            var context = new TypeContext(new Settings
+            {
+                UseNativeDates = false
+            });
+
+            var resolvedType = context.GetTypeScriptType(typeof(DateTimeOffset).FullName);
+            Assert.IsInstanceOfType(resolvedType, typeof(StringType));
+        }
+    }
+}
