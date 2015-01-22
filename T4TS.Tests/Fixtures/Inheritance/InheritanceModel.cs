83777b3a5a3ce4f5cbd485d7c8e5c73ad64d7482
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-22 23:30:55 +0100

    Add inheritance fixture test

---
+++ T4TS.Tests/Fixtures/Inheritance/InheritanceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4TS.Example.Models;
+using T4TS.Tests.Fixtures.Basic;
+
+namespace T4TS.Tests.Fixtures.Inheritance
+{
+    [TypeScriptInterface]
+    public class InheritanceModel
+    {
+        public BasicModel Basic { get; set; }
+        public ModelFromDifferentProject External { get; set; }
+    }
+}
