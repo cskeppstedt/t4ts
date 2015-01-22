8345167fd13c0282804cbbb5768d50bcabeb84c2
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-22 21:37:08 +0100

    WIP basic fixture

---
+++ T4TS.Tests/Fixtures/Basic/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TS.Tests.Fixtures.Basic
+{
+    [TypeScriptInterface]
+    public class Model
+    {
+        public int MyProperty { get; set; }
+    }
+}
