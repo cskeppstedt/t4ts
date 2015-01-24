a469a3fc9578892a2b15716756dbbfb3badb6856
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-24 01:08:41 +0100

    InheritanceModel is actually ExternalPropModel

--- T4TS.Tests/Fixtures/ExternalProp/ExternalPropModel.cs
+++ T4TS.Tests/Fixtures/ExternalProp/ExternalPropModel.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 using T4TS.Example.Models;
 using T4TS.Tests.Fixtures.Basic;
 
-namespace T4TS.Tests.Fixtures.Inheritance
+namespace T4TS.Tests.Fixtures.ExternalProp
 {
     [TypeScriptInterface]
-    public class InheritanceModel
+    public class ExternalPropModel
     {
         public BasicModel Basic { get; set; }
         public ModelFromDifferentProject External { get; set; }
