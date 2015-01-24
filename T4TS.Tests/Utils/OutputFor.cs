6322d97a99bba348302a82c6b088b3ac49d50b1d
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-24 01:03:55 +0100

    Add a helper for the fixture tests

---
+++ T4TS.Tests/Utils/OutputFor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace T4TS.Tests.Utils
+{
+    class OutputFor
+    {
+        readonly IReadOnlyCollection<Type> Types;
+
+        public OutputFor(params Type[] types)
+        {
+            this.Types = new ReadOnlyCollection<Type>(types);
+        }
+
+        public void ToEqual(string expectedOutput)
+        {
+            var generatedOutput = GenerateOutput();
+            Assert.AreEqual(Normalize(expectedOutput), Normalize(generatedOutput));
+        }
+
+        private string GenerateOutput()
+        {
+            var solution = DTETransformer.BuildDteSolution(this.Types.ToArray());
+            var settings = new Settings();
+            var generator = new CodeTraverser(solution, settings);
+            var data = generator.GetAllInterfaces().ToList();
+            
+            return OutputFormatter.GetOutput(data, settings);
+        }
+
+        static string Normalize(string output)
+        {
+            return Regex.Replace(output, @"\r\n|\n\r|\n|\r", "\n").Trim();
+        }
+    }
+}
