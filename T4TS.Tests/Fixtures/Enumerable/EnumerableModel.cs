b01d1bafba177668eb28c111e5352386a6346c1e
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-27 22:06:20 +0100

    Add enumerable fixture test

---
+++ T4TS.Tests/Fixtures/Enumerable/EnumerableModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4TS.Example.Models;
+using T4TS.Tests.Fixtures.Basic;
+
+namespace T4TS.Tests.Fixtures.Enumerable
+{
+    [TypeScriptInterface]
+    public partial class EnumerableModel
+    {
+        public int NormalProperty { get; set; }
+        public int[] PrimitiveArray { get; set; }
+        public List<int> PrimitiveList { get; set; }
+        public BasicModel[] InterfaceArray { get; set; }
+        public List<BasicModel> InterfaceList { get; set; }
+    }
+}
