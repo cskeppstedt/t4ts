9f8f526a2e22f2845370218d6dac23505da54cd8
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-01-30 22:18:28 +0100

    Add indexed fixture test

---
+++ T4TS.Tests/Fixtures/Indexed/IndexedModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4TS.Tests.Fixtures.Basic;
+
+namespace T4TS.Tests.Fixtures.Indexed
+{
+    [TypeScriptInterface]
+    public class IndexedComplexModel : List<BasicModel>
+    {
+        public int SomeProp { get; set; }
+    }
+
+    [TypeScriptInterface]
+    public class IndexedPrimitiveModel : List<string>
+    {
+        public int SomeProp { get; set; }
+    }
+}
