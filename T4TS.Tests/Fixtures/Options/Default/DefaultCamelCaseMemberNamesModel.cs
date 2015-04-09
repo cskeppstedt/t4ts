34d79e13f630bf2008838ce02f3fa7b37cd512b8
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-04-09 22:58:38 +0200

    Add DefaultCamelCase test

---
+++ T4TS.Tests/Fixtures/Options/Default/DefaultCamelCaseMemberNamesModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TS.Tests.Fixtures.Options.Default
+{
+    [TypeScriptInterface]
+    public class DefaultCamelCaseMemberNamesModel
+    {
+        public string SomeProp { get; set; }
+    }
+
+    [TypeScriptInterface]
+    public class DefaultCamelCaseMemberNamesOverrideModel
+    {
+        [TypeScriptMember(CamelCase=false)]
+        public string SomeProp { get; set; }
+    }
+}
