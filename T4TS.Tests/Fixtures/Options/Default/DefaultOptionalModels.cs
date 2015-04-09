9ec69a8f2e7bbaa65c67680c5da9cff3de59ef2f
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-04-09 22:59:27 +0200

    Add DefaultOptional test

---
+++ T4TS.Tests/Fixtures/Options/Default/DefaultOptionalModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TS.Tests.Fixtures.Options.Default
+{
+    [TypeScriptInterface]
+    public class DefaultOptionalModel
+    {
+        public string SomeProp { get; set; }
+    }
+
+    [TypeScriptInterface]
+    public class DefaultOptionalOverrideModel
+    {
+        [TypeScriptMember(Optional = false)]
+        public string SomeProp { get; set; }
+    }
+}
