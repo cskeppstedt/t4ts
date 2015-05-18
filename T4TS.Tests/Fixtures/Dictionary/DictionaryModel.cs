38bd611fffb68500f9422825d28e524ee1f69b95
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-05-17 22:23:19 -0400

    Add tests and dictionary model

---
+++ T4TS.Tests/Fixtures/Dictionary/DictionaryModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using T4TS.Tests.Fixtures.Basic;
+
+namespace T4TS.Tests.Fixtures.Dictionary
+{
+    [TypeScriptInterface]
+    public class DictionaryModel : List<BasicModel>
+    {
+        public Dictionary<int, BasicModel> IntKey { get; set; }
+        public IDictionary<string, BasicModel> StringKey { get; set; }
+    }
+}
