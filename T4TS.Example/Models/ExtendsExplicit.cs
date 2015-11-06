a1e948f02b312917f5d03c1447ad61839d633043
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-11-06 10:28:38 +0100

    Rename exampe file

--- T4TS.Example/Models/ExtendsExplicit.cs
+++ T4TS.Example/Models/ExtendsExplicit.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace T4TS.Example.Models
 {
     [TypeScriptInterface(Name = "someEntity", Extends = "externalJSModule.Entity")]
-    public class SomeEntity
+    public class ExtendsExplicit
     {
         public int Number { get; set; }
         public string Name { get; set; }
