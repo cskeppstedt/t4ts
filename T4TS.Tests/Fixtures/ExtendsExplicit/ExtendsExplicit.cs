62b92f5c9e4f059431d4f9a749698abf290bb380
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-11-06 10:29:00 +0100

    Add test for explicitly extending

---
+++ T4TS.Tests/Fixtures/ExtendsExplicit/ExtendsExplicit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4TS.Example.Models;
+using T4TS.Tests.Fixtures.Basic;
+
+namespace T4TS.Tests.Fixtures.ExtendsExplicit
+{
+    [TypeScriptInterface(Extends="SomeFooBar")]
+    public class ExtendsExplicitModel
+    {
+        public BasicModel Basic { get; set; }
+    }
+}
