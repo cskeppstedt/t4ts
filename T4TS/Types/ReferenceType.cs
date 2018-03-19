a6e635b43fd87707a6f36fb95531036723b14506
Bryan Kraus <bkraus@panopto.com>
2018-03-19 16:03:28 -0700

    Rename file

---
+++ T4TS/Types/ReferenceType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TS
+{
+    public class InterfaceType : TypescriptType
+    {
+        TypeScriptInterface tsInterface;
+
+        public string QualifedModule 
+        { 
+            get 
+            {
+                return (this.tsInterface.Module != null)
+                    ? this.tsInterface.Module.QualifiedName
+                    : null;
+            } 
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return this.tsInterface.Name;
+            }
+        }
+
+        public InterfaceType(TypeScriptInterface tsInterface)
+        {
+            this.tsInterface = tsInterface;
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(QualifedModule))
+                return base.ToString();
+
+            return QualifedModule + "." + base.ToString();
+        }
+    }
+}
