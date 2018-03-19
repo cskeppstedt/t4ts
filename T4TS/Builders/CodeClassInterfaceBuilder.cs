f9c0e6313250740d0c7eda0528637177f6b78122
Bryan Kraus <bkraus@panopto.com>
2018-03-19 15:44:26 -0700

    Rename File

---
+++ T4TS/Builders/CodeClassInterfaceBuilder.cs
@@ -0,0 +1,16 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TS.Builders
+{
+    public interface CodeClassInterfaceBuilder
+    {
+        TypeScriptInterface Build(
+            CodeClass codeClass,
+            TypeContext typeContext);
+    }
+}
