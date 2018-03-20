c44465185f37ca12f07670adc428938b7cb3f063
Bryan Kraus <bkraus@panopto.com>
2018-03-20 16:27:40 -0700

    Test of same namespace in multiple projects

---
+++ T4TS.Example.Models/DifferentProjectSameNamespace.cs
@@ -0,0 +1,8 @@
+﻿namespace T4TS.Tests.Fixtures.Basic
+{
+    [TypeScriptInterface(Module="T4TS")]
+    public class DifferentProjectSameNamespace
+    {
+        public int Id { get; set; }
+    }
+}
